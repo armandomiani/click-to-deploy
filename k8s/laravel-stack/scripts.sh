7f4eeedff2f58f8a147b66202af0d824f760a6f9
Armando Miani <armadom@google.com>
2020-04-11 08:05:15 +0200

    [WIP] test helm templte

--- k8s/laravel-stack/scripts.sh
+++ k8s/laravel-stack/scripts.sh
@@ -7,11 +7,23 @@ function apply_app() {
 }
 
 function template() {
+    export TLS_CERTIFICATE_KEY="$(cat /tmp/tls.key | base64)"
+    export TLS_CERTIFICATE_CRT="$(cat /tmp/tls.crt | base64)"
+
     helm template chart/laravel-stack \
         --name "${APP_INSTANCE_NAME}" \
         --namespace "${NAMESPACE}" \
+        --set nginx.tls.base64EncodedPrivateKey="$TLS_CERTIFICATE_KEY" \
+        --set nginx.nginx.tls.base64EncodedCertificate="$TLS_CERTIFICATE_CRT" \
         > ${APP_INSTANCE_NAME}_manifest.yaml
   echo "Template OK!"
+}
+
+function create_cert() {
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /tmp/tls.key \
+        -out /tmp/tls.crt \
+        -subj "/CN=nginx/O=nginx"
 }
 
 function apply() {
--- k8s/laravel-stack/temp-deployer.sh
+++ k8s/laravel-stack/temp-deployer.sh
@@ -1,25 +1,58 @@
 #!/bin/bash
 
-destination_folder="chart/deps/charts"
-if [[ ! -d "${destination_folder}" ]]; then
-    mkdir -p "${destination_folder}"
+while [[ "$#" != 0 ]]; do
+  case "$1" in
+    --command)
+      command="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unsupported flag: $1 - EXIT"
+      exit 1
+  esac
+done;
+
+function build() {
+  destination_folder="chart/deps/charts"
+  if [[ ! -d "${destination_folder}" ]]; then
+      mkdir -p "${destination_folder}"
+  fi
+
+  for solution in $(cat chart/deps/deps.json | jq -r '.[].solution'); do
+      source_folder="../${solution}/chart/${solution}"
+      solution_destination_folder="${destination_folder}/${solution}"
+
+      current_dir="$(pwd)"
+      current_solution="${current_dir##*/}"
+
+      if [[ ! -d "${solution_destination_folder}" ]]; then
+          mkdir -p "${solution_destination_folder}"
+      fi
+
+      echo "Importing ${solution}..."
+      echo "Copying files from: ${source_folder}"
+      cp -Rfv "${source_folder}/" "$(pwd)/chart/deps/charts/"
+
+      echo "Removing: $(pwd)/chart/deps/charts/${solution}/templates/application.yaml"
+      rm -fv "$(pwd)/chart/deps/charts/${solution}/templates/application.yaml"
+      # rm -fv "$(pwd)/chart/deps/charts/${solution}/Chart.yaml"
+      # rm -fv "$(pwd)/chart/deps/charts/${solution}/values.yaml"
+      # rm -fv "$(pwd)/chart/deps/charts/${solution}/logo.png"
+
+
+      # cp -Rf "${solution_destination_folder}/" "$(pwd)/chart/${current_solution}/templates"
+  done
+
+  cp -Rf "${destination_folder}" "$(pwd)/chart/${current_solution}"
+}
+
+function destroy() {
+ rm -rfv chart/deps/charts
+}
+
+if [[ "${command}" == "build" ]]; then
+  build
+  # destroy
+elif [[ "${command}" == "destroy" ]]; then
+  destroy
 fi
-
-for solution in $(cat chart/deps/deps.json | jq -r '.[].solution'); do
-    source_folder="../${solution}/chart/${solution}/templates"
-    solution_destination_folder="${destination_folder}/${solution}"
-
-    if [[ ! -d "${solution_destination_folder}" ]]; then
-        mkdir -p "${solution_destination_folder}"
-    fi
-
-    echo "Importing ${solution}..."
-    echo "Copying files from: ${source_folder}"
-
-    find ${source_folder} -type f ! -name application.yaml -exec cp -f -v -t "${solution_destination_folder}" {} +
-
-    current_dir="$(pwd)"
-    current_solution="${current_dir##*/}"
-
-    cp -Rf "${solution_destination_folder}/" "$(pwd)/chart/${current_solution}/templates"
-done
